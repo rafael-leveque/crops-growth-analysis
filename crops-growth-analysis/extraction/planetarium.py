b229cdcc5a36de8cb0c1a1905f2947c3aae17605
Rafael Leveque <rafael.leveque@gmail.com>
2024-07-18 12:52:30 +0200

    extraction : first planetarium

---
+++ crops-growth-analysis/extraction/planetarium.py
@@ -0,0 +1,18 @@
+import planetary_computer
+from pystac_client import Client, ItemSearch
+from shapely import Polygon
+
+class Planetarium:
+
+    def __init__(self):
+        self.__catalog = Client.open(
+            "https://planetarycomputer.microsoft.com/api/stac/v1",
+            modifier=planetary_computer.sign_inplace,
+        )
+
+    def search_polygon(self, polygon: Polygon) -> ItemSearch:
+        return self.__catalog.search(
+            collections=["sentinel-2-l2a"],
+            intersects=polygon,
+            datetime="2020-01-01/2020-12-31",
+        )
