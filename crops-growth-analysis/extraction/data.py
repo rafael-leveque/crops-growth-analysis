6ef6bdf4226af465da97a30e80a093205168bb0f
Rafael Leveque <rafael.leveque@gmail.com>
2024-07-18 11:39:32 +0200

    read and display data

---
+++ crops-growth-analysis/extraction/data.py
@@ -0,0 +1,53 @@
+
+# Read data csv without panda
+
+import csv
+import shapely
+from shapely.geometry import Polygon
+import matplotlib.pyplot as plt
+
+def read_csv(file_path: str) -> list[tuple[str, Polygon]]:
+    """
+    Read a parcel csv file and keep only the id and geometry columns
+    Return a list of parcels with an ID and a polygon
+    """
+    data: list[tuple[str, Polygon]] = []
+    with open(file_path, 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            id = row[1]
+            polygon = shapely.from_wkt(row[8])
+            data.append((id, polygon))
+    return data
+
+def read_maize_data():
+    """
+    Read maize data from csv file
+    Result is a list of parcels with an ID and a polygon
+    """
+    return read_csv("data/maize.csv")
+
+def read_tournesol_data():
+    """
+    Read tournesol data from csv file
+    Result is a list of parcels with an ID and a polygon
+    """
+    return read_csv("data/tournesol.csv")
+
+def display_data(title: str, data: list[tuple[str, Polygon]]):
+    """
+    Plot the data on a map
+    """
+    plt.figure()
+    for id, polygon in data:
+        x, y = polygon.exterior.xy
+        center = polygon.centroid
+
+        # Draw polygon and label it
+        plt.plot(x, y, color='blue', linewidth=2)
+        plt.fill(x, y, color='skyblue', alpha=0.5)
+        plt.text(center.x, center.y, 'Polygon', fontsize=12, ha='center', va='center', color='black')
+
+    plt.title(f'{title} parcels')
+    plt.grid(True)
+    plt.show()
---
+++ crops-growth-analysis/main.py
@@ -0,0 +1,12 @@
+import extraction
+import extraction.data
+
+def main():
+    maize_data = extraction.data.read_maize_data()
+    extraction.data.display_data("Maize", maize_data)
+
+    tournesol_data = extraction.data.read_tournesol_data()
+    extraction.data.display_data("Tournesol", tournesol_data)
+
+if __name__ == "__main__":
+    main()
