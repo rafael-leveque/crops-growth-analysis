202a61193eb3b0ef2ac07c85b34aca507dd7ce6d
Rafael Leveque <rafael.leveque@gmail.com>
2024-07-18 12:52:09 +0200

    Extraction : rename to fields

--- crops-growth-analysis/extraction/data.py
+++ crops-growth-analysis/extraction/data.py
@@ -2,16 +2,23 @@
 # Read data csv without panda
 
 import csv
+from dataclasses import dataclass
 import shapely
 from shapely.geometry import Polygon
 import matplotlib.pyplot as plt
 
-def read_csv(file_path: str) -> list[tuple[str, Polygon]]:
+@dataclass
+class Field:
+    id: str
+    polygon: Polygon
+    planetarium_data = None
+
+def read_csv(file_path: str) -> list[Field]:
     """
     Read a parcel csv file and keep only the id and geometry columns
     Return a list of parcels with an ID and a polygon
     """
-    data: list[tuple[str, Polygon]] = []
+    fields: list[Field] = []
     with open(file_path, 'r') as file:
         reader = csv.reader(file)
         # Skip header
@@ -19,29 +26,29 @@ def read_csv(file_path: str) -> list[tuple[str, Polygon]]:
         for row in reader:
             id = row[0]
             polygon = shapely.from_wkt(row[7])
-            data.append((id, polygon))
-    return data
+            fields.append(Field(id, polygon))
+    return fields
 
-def read_maize_data():
+def read_maize_fields():
     """
     Read maize data from csv file
     Result is a list of parcels with an ID and a polygon
     """
     return read_csv("data/maize.csv")
 
-def read_tournesol_data():
+def read_tournesol_fields():
     """
     Read tournesol data from csv file
     Result is a list of parcels with an ID and a polygon
     """
     return read_csv("data/tournesol.csv")
 
-def display_data(title: str, data: list[tuple[str, Polygon]]):
+def display_fields(title: str, fields: list[Field]):
     """
     Plot the data on a map
     """
     plt.figure()
-    for id, polygon in data:
+    for id, polygon in fields:
         x, y = polygon.exterior.xy
         center = polygon.centroid
 
@@ -50,6 +57,6 @@ def display_data(title: str, data: list[tuple[str, Polygon]]):
         plt.fill(x, y, color='skyblue', alpha=0.5)
         plt.text(center.x, center.y, id, fontsize=12, ha='center', va='center', color='black')
 
-    plt.title(f'{title} parcels')
+    plt.title(f'{title} fields')
     plt.grid(True)
     plt.show()
