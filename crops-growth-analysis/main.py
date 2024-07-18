b992bbeb26415483d8debe247e78ddc883316216
Rafael Leveque <rafael.leveque@gmail.com>
2024-07-18 12:52:41 +0200

    Extraction : update main

--- crops-growth-analysis/main.py
+++ crops-growth-analysis/main.py
@@ -1,14 +1,23 @@
-import extraction
-import extraction.data
+from extraction import data
+from extraction.planetarium import Planetarium
 
 def main():
-    maize_data = extraction.data.read_maize_data()
-    extraction.data.display_data("Maize", maize_data)
+    # Read csv data
+    maize_fields = data.read_maize_fields()
+    tournesol_fields = data.read_tournesol_fields()
 
-    tournesol_data = extraction.data.read_tournesol_data()
-    extraction.data.display_data("Tournesol", tournesol_data)
+    # Display data
+    # extraction.data.display_data("Maize", maize_data)
+    # extraction.data.display_data("Tournesol", tournesol_data)
+    # extraction.data.display_data("All", maize_data + tournesol_data)
 
-    extraction.data.display_data("All", maize_data + tournesol_data)
+    # Search planetarium data
+    planetarium = Planetarium()
+    sample = maize_fields[0]
+    planetarium_sample = planetarium.search_polygon(sample.polygon).get_all_items()
+    p_item = planetarium_sample[0]
+    print(p_item)
+
 
 if __name__ == "__main__":
     main()
