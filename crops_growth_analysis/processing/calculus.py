9084fbae93f0409fe3dbd3ffdd475b25b8935846
Rafael Leveque <rafael.leveque@gmail.com>
2024-07-20 15:16:14 +0200

    Processing : v1

--- crops_growth_analysis/processing/calculus.py
+++ crops_growth_analysis/processing/calculus.py
@@ -1,14 +1,42 @@
 """Module to calculate NDVI and NDMI from Sentinel-2 data"""
 
+import numpy
 import xarray
+
+from logger import log
+
+
+def format_as_dataset(data: dict[str, xarray.DataArray]) -> xarray.Dataset:
+    """
+    Make the required changes to the data so that it can be used as a dataset
+    """
+    # B11 and SCL are half the definition of B04 and B08
+    # We need to upscale them
+    log.info("Upscaling B11 and SCL")
+    data["B11"] = upscale_array(data["B11"])
+    data["SCL"] = upscale_array(data["SCL"])
+    return xarray.Dataset(data)
+
+
+def upscale_array(da: xarray.DataArray) -> xarray.DataArray:
+    """Double size of the array, using linear interpolation"""
+    return da.interp(
+        coords={
+            k: numpy.linspace(0, v - 1, v * 2) for k, v in da.sizes.items()
+        }
+    ).assign_coords(
+        coords={k: numpy.arange(v * 2) for k, v in da.sizes.items()}
+    )
 
 
 def ndvi_and_ndmi(ds: xarray.Dataset) -> xarray.Dataset:
     """Filter on SCL and calculate NDVI and NDMI"""
+    log.info("Filtering on SCL")
     ds_clean = ds.where(ds["SCL"] < 7 & ds["SCL"] > 1)
     nir = ds_clean["B08"]
     red = ds_clean["B04"]
     swir = ds_clean["B11"]
+    log.info("Calculating NDVI and NDMI")
     ndvi = (nir - red) / (nir + red)
     ndmi = (nir - swir) / (nir + swir)
     return xarray.Dataset({"NDVI": ndvi, "NDMI": ndmi})
--- crops_growth_analysis/processing/images.py
+++ crops_growth_analysis/processing/images.py
@@ -7,21 +7,28 @@ import requests
 import xarray
 from PIL import Image
 from pystac import Item
+from logger import log
 
 
-def get_bands_datasets(sentinel_item: Item) -> xarray.Dataset:
-    """Get bands datasets"""
-    red = get_image(sentinel_item, "B04")
-    nir = get_image(sentinel_item, "B08")
-    swir = get_image(sentinel_item, "B11")
-    return xarray.Dataset({"B04": red, "B08": nir, "B11": swir})
+Image.MAX_IMAGE_PIXELS = None
 
 
-def get_image(sentinel_item: Item, band: str) -> xarray.DataArray:
+def get_bands(sentinel_item: Item) -> dict[str, xarray.DataArray]:
+    """Get bands dataarray from sentinel data"""
+    return {
+        band: load_band(sentinel_item, band)
+        for band in ["B04", "B08", "B11", "SCL"]
+    }
+
+
+def load_band(sentinel_item: Item, band: str) -> xarray.DataArray:
     """Get image from sentinel data"""
+    log.info(f"Loading image {band}")
     url = sentinel_item.assets[band].href
     raw_image = requests.get(url, stream=True, timeout=10).raw
+    log.info("Reading image")
     image = Image.open(raw_image)
+    log.info("Converting image to array")
     image_array = numpy.array(image)
     data_array = xarray.DataArray(image_array, dims=["y", "x"], name=band)
     return data_array
