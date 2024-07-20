07872ec361b3dac29495a7d90c66c4300c021276
Rafael Leveque <rafael.leveque@gmail.com>
2024-07-20 15:15:37 +0200

    Global: add logger

--- crops_growth_analysis/main.py
+++ crops_growth_analysis/main.py
@@ -2,13 +2,9 @@
 Main script to run the crops growth analysis.
 """
 
-import logging
-
 from extraction import parcels, sentinel
+from logger import log
 from processing import calculus, images
-
-logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(message)s")
 
 PARCEL_LIMIT = 1
 
@@ -18,14 +14,14 @@ def main():
     Main function to run the crops growth analysis.
     """
     # Read csv parcels
-    logging.info("--- Prepare Data ---")
+    log.info("--- Prepare Data ---")
 
-    logging.info("Reading CSV (Limited to %s parcels)", PARCEL_LIMIT)
+    log.info("Reading CSV (Limited to %s parcels)", PARCEL_LIMIT)
     maize_parcels = parcels.read_maize()[:PARCEL_LIMIT]
 
-    # logging.info("Reading tournesol parcels")
+    # log.info("Reading tournesol parcels")
     # tournesol_parcels = parcels.read_tournesol()
-    # logging.info(f"Tournesol parcels: {len(tournesol_parcels)}")
+    # log.info(f"Tournesol parcels: {len(tournesol_parcels)}")
 
     # Display parcels
     # extraction.parcels.display_parcels("Maize", maize_parcels)
@@ -33,19 +29,22 @@ def main():
     # extraction.parcels.display_parcels("All", maize_data + tournesol_parcels)
 
     # Search planetarium data
-    logging.info("Searching planetarium data")
+    log.info("Searching planetarium data")
     for parcel in maize_parcels:
         parcel.sentinel_data = sentinel.search_polygon(parcel.wgs64_polygon)
 
     # Get SCL image
-    logging.info("Calculating NDVI and NDMI")
+    log.info("Calculating NDVI and NDMI")
     for parcel in maize_parcels:
+        log.info(f"Processing parcel {parcel.id}")
         parcel.ndvi = []
         for sentinel_data in parcel.sentinel_data:
-            logging.info(f"Loading images for parcel {parcel.id}")
-            dataset = images.get_bands_datasets(sentinel_data)
-            logging
-            result = calculus.ndvi_and_ndmi(dataset)
+            log.info("Loading images")
+            bands_da = images.get_bands(sentinel_data)
+            log.info("Formatting as dataset")
+            bands_ds = calculus.format_as_dataset(bands_da)
+            log.info("Calculating NDVI and NDMI")
+            result = calculus.ndvi_and_ndmi(bands_ds)
             parcel.ndvi.append(result)
 
 
