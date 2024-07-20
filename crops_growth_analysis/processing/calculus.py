63dca0690190ac7640c7eea898d9f5b0202e21d5
Rafael Leveque <rafael.leveque@gmail.com>
2024-07-20 16:11:40 +0200

    Processing : add choice in upscale for tests

--- crops_growth_analysis/processing/calculus.py
+++ crops_growth_analysis/processing/calculus.py
@@ -2,23 +2,66 @@
 
 import numpy
 import xarray
-
 from logger import log
+from processing import images
+from pystac import Item
 
 
-def format_as_dataset(data: dict[str, xarray.DataArray]) -> xarray.Dataset:
-    """
-    Make the required changes to the data so that it can be used as a dataset
-    """
-    # B11 and SCL are half the definition of B04 and B08
-    # We need to upscale them
-    log.info("Upscaling B11 and SCL")
-    data["B11"] = upscale_array(data["B11"])
-    data["SCL"] = upscale_array(data["SCL"])
-    return xarray.Dataset(data)
+def get_scl(sentinel_data: Item) -> xarray.DataArray:
+    """Filter out clouds from SCL"""
+    log.info("Loading SCL")
+    scl = images.load_band(sentinel_data, "SCL")
+    log.info("Filtering SCL")
+    scl = scl.where(scl < 7 & scl > 1)
+    log.info("Upscaling SCL")
+    return upscale_array(scl)
+
+
+def get_nir(sentinel_data: Item) -> xarray.DataArray:
+    """Get NIR band from Sentinel data"""
+    log.info("Loading NIR")
+    return images.load_band(sentinel_data, "B08")
+
+
+def get_ndvi(
+    sentinel_data: Item, scl: xarray.DataArray, nir: xarray.DataArray
+) -> xarray.DataArray:
+    """Get NDVI from Sentinel data"""
+    log.info("Calculating NDVI")
+    log.info("Filtering NIR")
+    nir = nir.where(scl)
+    log.info("Loading RED")
+    red = images.load_band(sentinel_data, "B04")
+    log.info("Filtering RED")
+    red = red.where(scl)
+    log.info("Calculate NDVI")
+    return (nir - red) / (nir + red)
+
+
+def get_ndmi(
+    sentinel_data: Item, scl: xarray.DataArray, nir: xarray.DataArray
+) -> xarray.DataArray:
+    """Get NDMI from Sentinel data"""
+    log.info("Calculating NDMI")
+    log.info("Filtering NIR")
+    nir = nir.where(scl)
+    log.info("Loading SWIR")
+    swir = images.load_band(sentinel_data, "B11")
+    log.info("Filtering SWIR")
+    swir = swir.where(scl)
+    log.info("Upscaling SWIR")
+    swir = upscale_array(swir)
+    log.info("Calculate NDMI")
+    return (nir - swir) / (nir + swir)
 
 
 def upscale_array(da: xarray.DataArray) -> xarray.DataArray:
+    """Double size of the array, using linear interpolation"""
+    # return upscale_using_interpolation(da)
+    return upscale_using_reindex(da)
+
+
+def upscale_using_interpolation(da: xarray.DataArray) -> xarray.DataArray:
     """Double size of the array, using linear interpolation"""
     return da.interp(
         coords={
@@ -29,14 +72,11 @@ def upscale_array(da: xarray.DataArray) -> xarray.DataArray:
     )
 
 
-def ndvi_and_ndmi(ds: xarray.Dataset) -> xarray.Dataset:
-    """Filter on SCL and calculate NDVI and NDMI"""
-    log.info("Filtering on SCL")
-    ds_clean = ds.where(ds["SCL"] < 7 & ds["SCL"] > 1)
-    nir = ds_clean["B08"]
-    red = ds_clean["B04"]
-    swir = ds_clean["B11"]
-    log.info("Calculating NDVI and NDMI")
-    ndvi = (nir - red) / (nir + red)
-    ndmi = (nir - swir) / (nir + swir)
-    return xarray.Dataset({"NDVI": ndvi, "NDMI": ndmi})
+def upscale_using_reindex(da: xarray.DataArray) -> xarray.DataArray:
+    """Double size of the array, using reindex"""
+    return da.assign_coords(
+        coords={k: numpy.arange(v * 2, step=2) for k, v in da.sizes.items()}
+    ).reindex(
+        coords={k: numpy.arange(v * 2) for k, v in da.sizes.items()},
+        method="ffill",
+    )
