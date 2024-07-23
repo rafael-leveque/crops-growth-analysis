f62eb215abd6c1333dcb13ef7204b6be21642968
Rafael Leveque <rafael.leveque@gmail.com>
2024-07-23 10:25:24 +0200

    Store : add store task to main

--- crops_growth_analysis/main.py
+++ crops_growth_analysis/main.py
@@ -6,12 +6,14 @@ import time
 
 from crops_growth_analysis.extract import csv, sentinel
 from crops_growth_analysis.logger import log
-from crops_growth_analysis.process import external
-
 # from crops_growth_analysis.process import manual
+from crops_growth_analysis.process import external
+# from crops_growth_analysis.store import minio
+# from crops_growth_analysis.store import mongodb
+from crops_growth_analysis.store import postgresql
 
-PARCEL_LIMIT = 1
-ASSETS_LIMIT = 5
+PARCEL_LIMIT = -1
+ASSETS_LIMIT = -1
 
 
 def main():
@@ -19,7 +21,9 @@ def main():
     Main function to run the crops growth analysis.
     """
     # Start Timer
-    log.info("--- Start Timer ---")
+    overall_start_time = time.time()
+
+    log.info("--- Start Extract ---")
     start_time = time.time()
 
     # Read csv parcels
@@ -43,27 +47,46 @@ def main():
             :ASSETS_LIMIT
         ]
 
+    extract_time = time.time() - start_time
+    log.info("--- Extract Time : %s ---", extract_time)
+    log.info("--- Start Process ---")
+    start_time = time.time()
+
     log.info("Calculating NDVI and NDMI")
     for parcel in parcels:
         log.debug("Processing parcel %s", parcel.id)
         # parcel.bands = manual.process_parcel(parcel)
         parcel.bands = external.process_parcel(parcel).compute()
 
-    for parcel in parcels:
-        log.info("Storing parcel %s", parcel.id)
-        for band in parcel.bands:
-            log.info("Storing band %s", band["band"].item())
-            for datetime in band:
-                log.info("Storing time %s", datetime["time"].item())
-            exit(0)
-        # storage.minio.store_parcel(parcel)
-        # storage.mongodb.store_parcel(parcel)
-    log.info("--- End Timer ---")
+    process_time = time.time() - start_time
+    log.info("--- Process Time : %s ---", process_time)
     log.info(
         "--- DataArray Size: %s kb ---",
         round(sum([parcel.bands.nbytes for parcel in parcels]) / 1024, 2),
     )
-    log.info("--- Execution time: %s seconds ---", time.time() - start_time)
+    log.info("--- Start Store ---")
+    start_time = time.time()
+
+    log.info("Starting DB connection")
+    storage = postgresql.ParcelStorage()
+    # storage = minio.ParcelStorage()
+    # storage = mongodb.ParcelStorage()
+    log.info("Storing parcels")
+    for parcel in parcels:
+        log.debug("Storing parcel %s", parcel.id)
+        storage.store_parcel(parcel)
+    log.info("Closing DB connection")
+    storage.close()
+
+    store_time = time.time() - start_time
+    log.info("--- Store Time : %s ---", store_time)
+
+    overall_time = time.time() - overall_start_time
+    log.info("--- Summary ---")
+    log.info("--- Extract Time : %s ---", extract_time)
+    log.info("--- Process Time : %s ---", process_time)
+    log.info("--- Store Time : %s ---", store_time)
+    log.info("--- Overall Time : %s ---", overall_time)
 
 
 if __name__ == "__main__":
